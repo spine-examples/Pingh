82342c375cc9d5ce89674bae18ec039daed91dc3
MykytaPimonovTD <mykyta.pimonov@teamdev.com>
2024-05-03 14:07:16 +0300

    Add group and version to each module.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,20 @@
+import io.spine.internal.dependency.Pingh
 import io.spine.internal.gradle.standardRepositories
-import io.spine.internal.standardRepositories
-
-buildscript {
-    standardRepositories()
-}
 
 plugins {
     kotlin("jvm")
 }
 
 allprojects {
+    apply(from = "$rootDir/version.gradle.kts")
+    version = extra["pinghVersion"]!!
+    group = Pingh.group
+
     repositories.standardRepositories()
+}
+
+subprojects {
+    apply {
+        plugin("kotlin")
+    }
 }
