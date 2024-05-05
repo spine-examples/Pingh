ecf1dd587aab007b54dba0911a669746f1c6feea
MykytaPimonovTD <mykyta.pimonov@teamdev.com>
2024-05-05 10:04:27 +0300

    Create a special plugin for installing repositories. Delete unnecessary `Repositories.kt` file.

---
+++ buildSrc/src/main/kotlin/dependency-management.gradle.kts
@@ -0,0 +1,12 @@
+repositories {
+    mavenLocal()
+    gradlePluginPortal()
+    mavenCentral()
+
+    maven {
+        url = uri("https://spine.mycloudrepo.io/public/repositories/releases")
+        mavenContent {
+            releasesOnly()
+        }
+    }
+}
