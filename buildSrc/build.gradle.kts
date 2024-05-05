216fddea9d1de77c2aa5cc1579af9a2a7a0b2fa1
MykytaPimonovTD <mykyta.pimonov@teamdev.com>
2024-05-05 10:20:15 +0300

    Remove unnecessary dependencies from the `build.gradle.kts` in the `buildSrc` module.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,35 +1,13 @@
 plugins {
-    java
     `kotlin-dsl`
 }
 
 repositories {
     mavenCentral()
     gradlePluginPortal()
+    mavenLocal()
 }
 
-/**
- * The version of the Kotlin Gradle plugin.
- */
-val kotlinVersion = "1.4.20"
-
-configurations.all {
-    resolutionStrategy {
-        force(
-            // Force Kotlin lib versions avoiding using those bundled with Gradle.
-            "org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion",
-            "org.jetbrains.kotlin:kotlin-stdlib-common:$kotlinVersion",
-            "org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion"
-        )
-    }
-}
-
-val jvmVersion = JavaLanguageVersion.of(17)
-
-java {
-    toolchain.languageVersion.set(jvmVersion)
-}
-
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+kotlin {
+    explicitApiWarning()
 }
