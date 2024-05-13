0f55b07905d9ca1bef99f0fe6c40f64623173d1e
MykytaPimonovTD <mykyta.pimonov@teamdev.com>
2024-05-13 13:35:03 +0300

    Create `UserSessionRepository`.

---
+++ sessions/src/main/kotlin/io/spine/examples/pingh/sessions/UserSessionRepository.kt
@@ -0,0 +1,28 @@
+package io.spine.examples.pingh.sessions
+
+import com.google.errorprone.annotations.OverridingMethodsMustInvokeSuper
+import io.spine.examples.pingh.sessions.event.UserLoggedIn
+import io.spine.examples.pingh.sessions.event.UserLoggedOut
+import io.spine.server.procman.ProcessManagerRepository
+import io.spine.server.route.EventRoute.withId
+import io.spine.server.route.EventRouting
+
+/**
+ * The repository for managing [UserSessionProcess] instances.
+ */
+public class UserSessionRepository :
+    ProcessManagerRepository<SessionId, UserSessionProcess, UserSession>() {
+
+    @OverridingMethodsMustInvokeSuper
+    @Override
+    override fun setupEventRouting(routing: EventRouting<SessionId>) {
+        super.setupEventRouting(routing)
+        routing
+            .route(UserLoggedIn::class.java) { event, context ->
+                withId(event.id)
+            }
+            .route(UserLoggedOut::class.java) { event, context ->
+                withId(event.id)
+            }
+    }
+}
