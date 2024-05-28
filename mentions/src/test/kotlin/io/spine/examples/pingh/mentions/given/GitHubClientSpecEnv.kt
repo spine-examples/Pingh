d0e4eb03cb4a51ee5851e1a16e8a34f2bd5643bc
MykytaPimonovTD <mykyta.pimonov@teamdev.com>
2024-05-28 18:10:02 +0300

    Update the getting of expected mentions of the user.

--- mentions/src/test/kotlin/io/spine/examples/pingh/mentions/given/GitHubClientSpecEnv.kt
+++ mentions/src/test/kotlin/io/spine/examples/pingh/mentions/given/GitHubClientSpecEnv.kt
@@ -27,13 +27,9 @@
 package io.spine.examples.pingh.mentions.given
 
 import com.google.protobuf.Timestamp
-import com.google.protobuf.util.Timestamps
 import io.spine.base.Time.currentTime
-import io.spine.examples.pingh.github.NodeId
 import io.spine.examples.pingh.github.PersonalAccessToken
-import io.spine.examples.pingh.github.User
 import io.spine.examples.pingh.github.Username
-import io.spine.examples.pingh.github.buildBy
 import io.spine.examples.pingh.mentions.GitHubClient
 import io.spine.examples.pingh.mentions.GitHubClientId
 import io.spine.examples.pingh.mentions.MentionId
@@ -43,10 +39,10 @@ import io.spine.examples.pingh.mentions.event.GitHubTokenUpdated
 import io.spine.examples.pingh.mentions.event.MentionsUpdateFromGitHubCompleted
 import io.spine.examples.pingh.mentions.event.MentionsUpdateFromGitHubRequested
 import io.spine.examples.pingh.mentions.event.UserMentioned
+import io.spine.examples.pingh.mentions.parseJson
 import io.spine.examples.pingh.mentions.rejection.GithubClientRejections.MentionsUpdateIsAlreadyInProgress
 import io.spine.examples.pingh.sessions.SessionId
 import io.spine.examples.pingh.sessions.event.UserLoggedIn
-import io.spine.net.Url
 import kotlin.reflect.KClass
 
 /**
@@ -132,58 +128,27 @@ internal fun KClass<MentionsUpdateIsAlreadyInProgress>.buildBy(id: GitHubClientI
         .setId(id)
         .vBuild()
 
-internal fun expectedUserMentionedSet(whoWasMentioned: Username): Set<UserMentioned> =
-    setOf(
-        UserMentioned::class.buildBy(
-            "PR_kwDOL2L5hc5vY_JP",
-            whoWasMentioned,
-            "MykytaPimonovTD",
-            "https://avatars.githubusercontent.com/u/160486193?v=4",
-            "Implement user session flow",
-            "2024-05-14T12:12:02Z",
-            "https://github.com/spine-examples/Pingh/pull/3"
-        ),
-        UserMentioned::class.buildBy(
-            "PR_kwDOL2L5hc5u-Jya",
-            whoWasMentioned,
-            "MykytaPimonovTD",
-            "https://avatars.githubusercontent.com/u/160486193?v=4",
-            "Define the ubiquitous language for Sessions and Mentions contexts",
-            "2024-05-09T09:49:43Z",
-            "https://github.com/spine-examples/Pingh/pull/2"
-        ),
-        UserMentioned::class.buildBy(
-            "PR_kwDOL2L5hc5udnm6",
-            whoWasMentioned,
-            "MykytaPimonovTD",
-            "https://avatars.githubusercontent.com/u/160486193?v=4",
-            "Set up Gradle configuration",
-            "2024-05-03T11:21:32Z",
-            "https://github.com/spine-examples/Pingh/pull/1"
-        )
-    )
-
 /**
- * Creates a new [UserMentioned] event with the specified [GitHubClientId].
+ * Reads mention data from a prepared JSON, converts it to [UserMentioned] events,
+ * and returns their set.
  */
-private fun KClass<UserMentioned>.buildBy(
-    nodeId: String,
-    whoWasMentioned: Username,
-    username: String,
-    avatarUr: String,
-    title: String,
-    timeInStringRepresentation: String,
-    url: String
-): UserMentioned =
-    UserMentioned.newBuilder()
-        .setId(
-            MentionId::class.buildBy(
-                NodeId::class.buildBy(nodeId),
-                whoWasMentioned
-            )
-        )
-        .setWhoMentioned(User::class.buildBy(username, avatarUr))
-        .setTitle(title)
-        .setWhenMentioned(Timestamps.parse(timeInStringRepresentation))
-        .setUrl(Url::class.buildBy(url))
-        .vBuild()
+internal fun expectedUserMentionedSet(whoWasMentioned: Username): Set<UserMentioned> {
+    val jsonFile = PredefinedGitHubResponses::class.java.getResource("github_response.json")
+    checkNotNull(jsonFile)
+    val json = jsonFile.readText(Charsets.UTF_8)
+    return parseJson(json)
+        .map { mention ->
+            with(UserMentioned.newBuilder()) {
+                id = MentionId::class.buildBy(
+                    mention.id,
+                    whoWasMentioned
+                )
+                whoMentioned = mention.author
+                title = mention.title
+                whenMentioned = mention.whenMentioned
+                url = mention.url
+                vBuild()
+            }
+        }
+        .toSet()
+}
