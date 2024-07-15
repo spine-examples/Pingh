13cb0cfd35c263ec0e6769075e1ef41cf595e51b
MykytaPimonovTD <mykyta.pimonov@teamdev.com>
2024-07-15 15:25:25 +0300

    Load icons to the project.

---
+++ desktop/src/main/kotlin/io/spine/examples/pingh/desktop/component/Icons.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2024, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.examples.pingh.desktop.component
+
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.res.loadImageBitmap
+import androidx.compose.ui.res.useResource
+
+/**
+ * Icons used in the Pingh desktop client.
+ *
+ * These icons are [free](https://bit.ly/3Tjmsqg) for personal and commercial
+ * use with attribution.
+ *
+ * Created by [Icon Hubs](https://www.flaticon.com/authors/icon-hubs).
+ */
+public object Icons {
+    public val snooze: BitmapPainter =
+        BitmapPainter(useResource("icons/snooze.png", ::loadImageBitmap))
+    public val profile: BitmapPainter =
+        BitmapPainter(useResource("icons/profile.png", ::loadImageBitmap))
+    public val refresh: BitmapPainter =
+        BitmapPainter(useResource("icons/refresh.png", ::loadImageBitmap))
+    public val back: BitmapPainter =
+        BitmapPainter(useResource("icons/back.png", ::loadImageBitmap))
+}
