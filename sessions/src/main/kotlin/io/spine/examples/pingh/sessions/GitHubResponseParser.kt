161b584d601de5bcb1291e8d105123874458af6d
MykytaPimonovTD <mykyta.pimonov@teamdev.com>
2024-07-29 13:24:40 +0300

    Create GitHub response parser.

---
+++ sessions/src/main/kotlin/io/spine/examples/pingh/sessions/GitHubResponseParser.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2024, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.examples.pingh.sessions
+
+import io.spine.examples.pingh.github.buildFromFragment
+import io.spine.examples.pingh.github.rest.AccessTokenFragment
+import io.spine.examples.pingh.github.rest.AccessTokenResponse
+import io.spine.examples.pingh.github.rest.AuthenticationCodesFragment
+import io.spine.examples.pingh.github.rest.AuthenticationCodesResponse
+import io.spine.json.Json
+
+/**
+ * Parses `AuthenticationCodesResponse` from the JSON.
+ */
+public fun parseAuthenticationCodesResponse(json: String): AuthenticationCodesResponse =
+    AuthenticationCodesResponse::class.buildFromFragment(
+        Json.fromJson(json, AuthenticationCodesFragment::class.java)
+    )
+
+/**
+ * Parses `AccessTokenResponse` from the JSON.
+ */
+public fun parseAccessTokenResponse(json: String): AccessTokenResponse =
+    AccessTokenResponse::class.buildFromFragment(
+        Json.fromJson(json, AccessTokenFragment::class.java)
+    )
