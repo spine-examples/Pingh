bbcf7619f5aab617505c39347176a2293dac7191
MykytaPimonovTD <mykyta.pimonov@teamdev.com>
2024-08-14 15:10:39 +0300

    Improve `testutil-sessions` build script.

--- testutil-sessions/build.gradle.kts
+++ testutil-sessions/build.gradle.kts
@@ -24,19 +24,6 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import io.spine.internal.BuildSettings
-
-plugins {
-    kotlin("jvm")
-}
-
-kotlin {
-    jvmToolchain {
-        languageVersion.set(BuildSettings.javaVersion)
-    }
-    explicitApi()
-}
-
 dependencies {
     implementation(project(":github"))
     implementation(project(":sessions"))
