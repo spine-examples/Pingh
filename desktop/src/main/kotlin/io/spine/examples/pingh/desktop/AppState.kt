2632f9f4fb0268d3f5885940b7a0512ddc3901b7
MykytaPimonovTD <mykyta.pimonov@teamdev.com>
2024-09-12 18:26:29 +0300

    Add server configurations to client app.

--- desktop/src/main/kotlin/io/spine/examples/pingh/desktop/AppState.kt
+++ desktop/src/main/kotlin/io/spine/examples/pingh/desktop/AppState.kt
@@ -30,6 +30,7 @@ import androidx.compose.ui.window.Notification
 import androidx.compose.ui.window.TrayState as ComposeTrayState
 import io.spine.examples.pingh.client.NotificationSender
 import io.spine.examples.pingh.client.PinghApplication
+import java.util.Properties
 
 /**
  * The top-level application state.
@@ -62,7 +63,29 @@ internal class AppState(settings: SystemSettings) {
 
     init {
         val notificationSender = TrayNotificationSender(composeTray) { !window.isShown }
-        app = PinghApplication(notificationSender)
+        val properties = loadProperties()
+        app = PinghApplication.builder()
+            .withAddress(properties.getProperty("server.address"))
+            .withPort(properties.getProperty("server.port").toInt())
+            .with(notificationSender)
+            .build()
+    }
+
+    /**
+     * Loads server properties from resource folder.
+     */
+    private fun loadProperties(): Properties {
+        val properties = Properties()
+        javaClass.getResourceAsStream("/config/server.properties").use {
+            properties.load(it)
+        }
+        check(properties.containsKey("server.address")) {
+            "The Pingh server address must be provided in the configuration file.."
+        }
+        check(properties.containsKey("server.port")) {
+            "The Pingh server port must be provided in the configuration file."
+        }
+        return properties
     }
 }
 
