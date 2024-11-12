7b7980a0a093db7dd3cb3d9f4f27c222871b4e34
MykytaPimonovTD <mykyta.pimonov@teamdev.com>
2024-11-12 15:41:11 +0200

    Add retrieve application data file from the user's home directory.

---
+++ client/src/main/kotlin/io/spine/examples/pingh/client/AppDataStorage.kt
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2024, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.examples.pingh.client
+
+import java.io.File
+import kotlin.io.path.Path
+import kotlin.io.path.createDirectories
+import kotlin.io.path.createFile
+import kotlin.io.path.exists
+import net.harawata.appdirs.AppDirsFactory
+
+internal object AppDataStorage {
+    private const val storageName = ".storage.json"
+    private val appDirPath = AppDirPath.withoutVersion()
+
+    /**
+     * Returns a file for storing the application state.
+     *
+     * If the file or its parent directories do not exist, they are created.
+     */
+    private fun storage(): File {
+        val parent = Path(appDirPath)
+        if (!parent.exists()) {
+            parent.createDirectories()
+        }
+        val child = parent.resolve(storageName)
+        if (!child.exists()) {
+            child.createFile()
+        }
+        return child.toFile()
+    }
+}
+
+/**
+ * Provides the path to platform-specific application data
+ * within the user’s home directory.
+ */
+private object AppDirPath {
+    private const val author = "spine-examples"
+    private const val name = "Pingh"
+    private const val version = "1.0.0"
+
+    /**
+     * Returns the path to the application data,
+     * without including the current application version.
+     */
+    fun withoutVersion(): String {
+        val versionedPath = AppDirsFactory.getInstance()
+            .getUserDataDir(name, version, author)
+        return versionedPath.substring(0, versionedPath.length - version.length - 1)
+    }
+}
