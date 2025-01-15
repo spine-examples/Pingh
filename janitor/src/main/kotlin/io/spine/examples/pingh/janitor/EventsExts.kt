6b04eee14117a0b1ae337c8b33a248e24aca88d1
MykytaPimonovTD <mykyta.pimonov@teamdev.com>
2025-01-15 16:57:18 +0200

    Declare janitor model.

---
+++ janitor/src/main/kotlin/io/spine/examples/pingh/janitor/EventsExts.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2025, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.examples.pingh.janitor
+
+import io.spine.examples.pingh.janitor.event.StorageCleaned
+import kotlin.reflect.KClass
+
+/**
+ * Creates a new `StorageCleaned` event with the passed ID of the janitor
+ * which removed obsolete data from the storage.
+ */
+@Suppress("UnusedReceiverParameter" /* Class extensions don't use class as a parameter. */)
+public fun KClass<StorageCleaned>.by(id: JanitorId): StorageCleaned =
+    StorageCleaned.newBuilder()
+        .setId(id)
+        .vBuild()
