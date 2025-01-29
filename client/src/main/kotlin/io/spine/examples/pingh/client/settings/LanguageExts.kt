b673ee663c65303b9d9b3d74d4873e7d43de712e
MykytaPimonovTD <mykyta.pimonov@teamdev.com>
2025-01-29 17:22:31 +0200

    Add `Language` property to user settings.

---
+++ client/src/main/kotlin/io/spine/examples/pingh/client/settings/LanguageExts.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2025, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.examples.pingh.client.settings
+
+import java.util.Locale
+import kotlin.reflect.KClass
+
+/**
+ * Returns a language name in its native form.
+ */
+public fun Language.nativeName(): String =
+    options[this]?.nativeName
+        ?: throw IllegalStateException("Language `${this}` is not currently supported by the app.")
+
+/**
+ * Returns a `Locale` representing a specific `Language`.
+ */
+internal fun Language.toLocale(): Locale {
+    val code = options[this]?.code
+        ?: throw IllegalStateException("Language `${this}` is not currently supported by the app.")
+    return Locale(code)
+}
+
+/**
+ * The list of languages currently supported by the app.
+ */
+@Suppress("UnusedReceiverParameter" /* Associated with the class but doesn't use its data. */)
+public val KClass<Language>.supported: List<Language>
+    get() = options.keys.toList().sortedBy { it.nativeName() }
+
+private val options = mapOf(
+    Language.ENGLISH to LanguageOption("English", "en"),
+    Language.UKRAINIAN to LanguageOption("Українська", "ua"),
+    Language.RUSSIAN to LanguageOption("Русский", "ru"),
+    Language.GERMAN to LanguageOption("Deutsch", "de")
+)
+
+/**
+ * Additional information related to [Language].
+ *
+ * @property nativeName The language name in its native form.
+ * @property code An ISO-639 alpha-2 or alpha-3 language code.
+ */
+private class LanguageOption(
+    val nativeName: String,
+    val code: String
+)
